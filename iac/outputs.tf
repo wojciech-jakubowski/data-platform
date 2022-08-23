424efcd86bc121fecd74b6b51a57988f8108249e
Wojciech Jakubowski <wojciech.jakubowski@lingarogroup.com>
2022-08-23 14:29:54 +0200

    + cleanup

--- iac/outputs.tf
+++ iac/outputs.tf
@@ -1,7 +1,5 @@
 output "output" {
   value = {
-    stroage = module.storage.output.dl_storage_account.containers
-    b       = module.storage.output.dl_storage_account.blob_endpoint
-    d       = module.storage.output.dl_storage_account.dfs_endpoint
+    #stroage = module.storage.output.dl_storage_account.containers
   }
 }
