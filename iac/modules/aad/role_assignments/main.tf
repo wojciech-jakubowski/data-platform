06f8ae62b1a183c7586b69f43dab17ff87c6c27f
Wojciech Jakubowski <wojciech.jakubowski@lingarogroup.com>
2022-08-30 11:59:40 +0200

    + role assignments

--- iac/modules/aad/role_assignments/main.tf
+++ iac/modules/aad/role_assignments/main.tf
@@ -4,3 +4,9 @@ resource "azurerm_role_assignment" "adf_on_storage" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id         = var.data_factory.adf.mi_id
 }
+
+resource "azurerm_role_assignment" "synapse_on_storage" {
+  scope                = var.storage.dl.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id         = var.synapse.workspace.mi_id
+}
