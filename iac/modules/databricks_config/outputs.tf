1a51e081ecb877441990d62915a4041d765ae815
Wojciech Jakubowski <wojciech.jakubowski@lingarogroup.com>
2022-09-05 14:31:12 +0200

    + cleanup

--- iac/modules/databricks_config/outputs.tf
+++ iac/modules/databricks_config/outputs.tf
@@ -1,8 +1,8 @@
 output "output" {
   value = {
     secrets = {
-      "SingleNodeClusterName" = databricks_cluster.single_node_cluster.cluster_name
-      "SingleNodeClusterId"   = databricks_cluster.single_node_cluster.id
+      # "SingleNodeClusterName" = databricks_cluster.single_node_cluster.cluster_name
+      # "SingleNodeClusterId"   = databricks_cluster.single_node_cluster.id
     }
   }
 }
