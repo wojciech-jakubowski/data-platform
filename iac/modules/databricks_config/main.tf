7bc2e56588f9420d87ad05d286e88cc0880fbeb3
Wojciech Jakubowski <wojciech.jakubowski@lingarogroup.com>
2022-10-12 11:47:08 +0200

    + fixes

--- iac/modules/databricks_config/main.tf
+++ iac/modules/databricks_config/main.tf
@@ -10,7 +10,7 @@ resource "databricks_cluster" "single_node_cluster" {
   cluster_name            = "Single Node"
   spark_version           = "10.4.x-scala2.12"
   node_type_id            = "Standard_DS3_v2"
-  autotermination_minutes = 15
+  autotermination_minutes = var.config.env == "dev" ? 60 : 15
   is_pinned               = true
 
   spark_conf = {
